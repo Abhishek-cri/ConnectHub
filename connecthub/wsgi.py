20e364baab1d0cd17f99e325d3d6c16a6c43c6de
Abhishek Kumar Prajapati <56184705+Abhishek-cri@users.noreply.github.com>
2023-03-17 12:50:42 +0530

    Update wsgi.py

--- connecthub/wsgi.py
+++ connecthub/wsgi.py
@@ -8,12 +8,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connecthub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
 
 application = get_wsgi_application()
-
-app=application
